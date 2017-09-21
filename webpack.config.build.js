c76f2c46eff1994dbbaf9e4aa9218a22f482c0a1
smendiratta <shashankmendiratta@outlook.com>
2017-09-21 09:03:26 +0530

    Reducing package size

--- webpack.config.build.js
+++ webpack.config.build.js
@@ -35,5 +35,8 @@ module.exports = {
     },
     plugins: [new webpack.DefinePlugin({
         'process.env.NODE_ENV': JSON.stringify('production')
-      })]
+      }),
+      new webpack.optimize.UglifyJsPlugin(), //minify everything
+      new webpack.optimize.AggressiveMergingPlugin()//Merge chunks 
+    ]
 }
